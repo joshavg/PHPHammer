51870ec0dd65e340f692bafd0aac928802be48bb
Josha von Gizycki <mail@joshavg.de>
2015-05-12 09:21:18 +0000

    removed test directory

--- phar.php
+++ phar.php
@@ -4,5 +4,3 @@ chdir(dirname(__FILE__));
 $phar = new Phar('bin/phphammer.phar', 0, 'phphammer.phar');
 $phar->buildFromDirectory('src');
 $phar->setDefaultStub('build.php');
-
-copy('bin/phphammer.phar', 'test/phphammer.phar');
