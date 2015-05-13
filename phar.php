6f2715f9b9330c2dfcfd0578701b8dcf47b58db2
Josha von Gizycki <mail@joshavg.de>
2015-05-13 14:24:45 +0000

    mkdir bin/

--- phar.php
+++ phar.php
@@ -1,6 +1,7 @@
 <?php
 
 chdir(dirname(__FILE__));
+@mkdir('bin');
 $phar = new Phar('bin/phphammer.phar', 0, 'phphammer.phar');
 $phar->buildFromDirectory('src');
 $phar->setDefaultStub('build.php');
