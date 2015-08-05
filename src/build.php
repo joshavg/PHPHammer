68de1ca468fec4ac2b8eff90a4a78d714b81ab20
Josha von Gizycki <mail@joshavg.de>
2015-08-05 13:16:12 +0000

    crrected time calculation

--- src/build.php
+++ src/build.php
@@ -69,7 +69,7 @@ if(!isset($json['targets'][$targetParam])) {
 Globals::set('buildfile', $json);
 Hammer::execTarget($targetParam);
 
-$duration = (microtime(true) - $start);
+$duration = microtime(true) - $start;
 
 Output::newln();
 Output::writeln("===== Duration: {$duration}s", Output::CYAN);
