d0d2aee3aa226ceb89c74a523bb84f929237883e
Josha von Gizycki <mail@joshavg.de>
2015-11-07 15:03:58 +0100

    phar creation as executable

--- phar.php
+++ phar.php
@@ -1,7 +1,16 @@
 <?php
 
 chdir(dirname(__FILE__));
+
 @mkdir('bin');
+@unlink('bin/phphammer.phar');
+
 $phar = new Phar('bin/phphammer.phar', 0, 'phphammer.phar');
+$phar->startBuffering();
+
+$defaultStub = $phar->createDefaultStub('build.php');
 $phar->buildFromDirectory('src');
-$phar->setDefaultStub('build.php');
+
+$stub = "#!/usr/bin/env php\n" . $defaultStub;
+$phar->setStub($stub);
+$phar->stopBuffering();
